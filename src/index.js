99db5743f39077c5d6ce02c2621f2ea80e8279a1
Radon4ik <Loboda.Radimir@gmail.com>
2022-06-03 23:10:56 +0300

    work is done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Fastify from 'fastify';
+import { user } from 'pg/lib/defaults';
 
 import { users } from './users';
 
@@ -11,4 +12,45 @@ fastify.register(import('@fastify/multipart'), {
 });
 fastify.register(import('@fastify/cookie'));
 
+fastify.post('/uppercase', (request, reply) => {
+  if (typeof request.body === 'string' && request.body.toUpperCase().indexOf('FUCK') === -1) {
+    return reply.send(request.body.toUpperCase())
+  } else {
+    return reply.status(403).send("unresolved")
+  }
+});
+
+fastify.post('/lowercase', (request, reply) => {
+  if (typeof request.body === 'string' && request.body.toUpperCase().indexOf('FUCK') === -1) {
+    return reply.send(request.body.toLowerCase())
+  } else {
+    return reply.status(403).send("unresolved")
+  }
+});
+
+fastify.get('/user/:id', (request, reply) => {
+  let { id } = request.params;
+  if (users[id]) {
+    return reply.send(users[id]);
+  } else {
+    return reply.status(400).send('User not exist')
+  }
+});
+
+
+fastify.get('/users', (request, reply) => {
+  let result = [];
+  const { filter, value } = request.query
+  Object.entries(users).filter((item) => {
+    if (item[1][filter] === value) {
+      result.push(item[1]);
+    } if (item[1][filter] === +value) {
+      result.push(item[1]);
+    }
+    return result;
+  })
+  return reply.send(result);
+})
+
 export default fastify;
+
